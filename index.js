c4b2025508a07d95e29b77be7207a8f6e28897b5
Anna Cole <acrrj123@gmail.com>
2023-05-22 15:08:49 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+
+  const configurationObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({ name, email })
+  };
+
+  return fetch('http://localhost:3000/users', configurationObj)
+  .then(resp => resp.json())
+  //.then(data => console.log(data))
+  .then(data => 
+    document.body.innerHTML += `<p>${data.id}</p>`)
+  .catch(error => 
+    document.body.innerHTML += `<p>${error.message}</p>`);
+};
+ 
+  
+  
+  
+
+
+
+
+
+
+
+
+
